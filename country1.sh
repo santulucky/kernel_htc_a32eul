c2f353b9a9c4aeb9c58afe9d63638c58b42320e3
bigcountry907 <belfiore.richard.james@gmail.com>
2016-10-16 17:32:23 -0400

    ADDED X-PHAT SUPPORT

--- country1.sh
+++ country1.sh
@@ -8,4 +8,6 @@ export SUBARCH=arm
 export CROSS_COMPILE=~/android/Toolchains/prebuilts/arm-eabi-4.8/bin/arm-eabi-
 make clean
 make mrproper
-make 1Big-Country_defconfig
+make 1Big-Country_defconfig -j8
+make prepare -j8
+make modules -j8
