03ce5c5109699719b0a2cbee94689b4dce4e01a3
bigcountry907 <belfiore.richard.james@gmail.com>
2016-10-16 20:54:40 -0400

    Added Pre-Built Boot Components

---
+++ post-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export KERNELOUT=~/kernel_htc_a32eul/out
+export KERNELMOD=~/kernel_htc_a32eul/out/modules
+export KERNELBOOT=~/kernel_htc_a32eul/out/boot
+export KERNELFLASH=~/kernel_htc_a32eul/out/flash
+
+mkdir -p $KERNELOUT
+mkdir -p $KERNELMOD
+mkdir -p $KERNELBOOT
+mkdir -p $KERNELFLASH
+
